c7f61017e63dab87e70e3a57548bdc3f5b5d3473
Hibaq Warsame <hibaq@ualberta.ca>
2023-03-22 02:46:19 -0600

    added basic styling for sign up page

--- Frontend/src/components/SignUp.js
+++ Frontend/src/components/SignUp.js
@@ -1,19 +1,18 @@
 function SignUp() {
   return (
     <div className='form-container'>
-       
-    <div className='message-container'>
-         <div className='message'>
-             <p>
-                 <b>Fit Cooking Back Into Your Schedule with </b>
-                 <img src='./logo-blk.png' alt="bug" height={40} />
-             </p>
-         </div>
-     </div>
-     <div className="form-card">
-         {/* <BasicExample /> */}
-         </div>
- </div>
+    
+                   <div className='message-container'>
+                        <div className='message'>
+                            <p>
+                                <b>Fit Cooking Back Into Your Schedule with </b>
+                                <img src='./logo-blk.png' alt="bug" height={40} />
+                            </p>
+                        </div>
+                    </div>
+                    <div className="form-card">
+                        </div>
+                </div>
   );
 }
 export default SignUp;
