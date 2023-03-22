208d28c1d917576ae4bcb86ac95852abaa7c8838
Hibaq Warsame <hibaq@ualberta.ca>
2023-03-22 03:45:06 -0600

    edited css class names in app.css

--- Frontend/src/components/SignUp.js
+++ Frontend/src/components/SignUp.js
@@ -1,18 +1,21 @@
+import SignUpForm from "./SignUpForm";
+
 function SignUp() {
   return (
     <div className='form-container'>
-    
-                   <div className='message-container'>
-                        <div className='message'>
-                            <p>
-                                <b>Fit Cooking Back Into Your Schedule with </b>
-                                <img src='./logo-blk.png' alt="bug" height={40} />
-                            </p>
-                        </div>
-                    </div>
-                    <div className="form-card">
-                        </div>
-                </div>
+
+      <div className='message-container'>
+        <div className='message'>
+          <p>
+            <b>Fit Cooking Back Into Your Schedule with </b>
+            <img src='./logo-blk.png' alt="bug" height={40} />
+          </p>
+        </div>
+      </div>
+      <div className="form-card">
+        <SignUpForm/>
+      </div>
+    </div>
   );
 }
 export default SignUp;
