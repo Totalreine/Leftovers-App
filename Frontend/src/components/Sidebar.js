af5d31d7df7cd8b3691d3f4bdafae027cbd244ab
Hibaq Warsame <hibaq@ualberta.ca>
2023-03-23 04:23:47 -0600

    created css file for sidebar and added links to sidebar menu

--- Frontend/src/components/Sidebar.js
+++ Frontend/src/components/Sidebar.js
@@ -1,5 +1,7 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import '../sidebar.css'
 import Fade from 'react-bootstrap/Fade';
 function Sidebar(props) {
   return (
@@ -8,11 +10,11 @@ function Sidebar(props) {
         <Row style={{ height: "100%", width: "25%", position: "absolute" }}>
           <Col xs={6} md={3} style={{ background: "#9E9BF2", height: "100%", width: "100%" }}>
             <ListGroup variant="flush">
-              <ListGroup.Item href="/home" style={{ background: "#9E9BF2" }}>
-                Home
+              <ListGroup.Item style={{ background: "#9E9BF2" }}>
+              <Link to="/home" className="menu-link">Home</Link>
               </ListGroup.Item>
-              <ListGroup.Item href="/myrecipes" style={{ background: "#9E9BF2" }}>
-                MyRecipes
+              <ListGroup.Item style={{ background: "#9E9BF2" }}>
+              <Link to="/myrecipes" className="menu-link">MyRecipes</Link>
               </ListGroup.Item>
             </ListGroup>
           </Col>
