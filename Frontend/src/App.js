89ce304563698697b4cedd5c0ce59f8cda080774
Hibaq Warsame <hibaq@ualberta.ca>
2023-03-24 15:17:44 -0600

    moved home,login, signup pages into routes folder and created login.css file

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar'
-import Home from './components/Home';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import SignUp from './routes/SignUp';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
---
+++ Frontend/src/routes/Login.js
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Login.css"
+import BasicExample from '../components/FormComp';
+function Login() {
+    return (
+            //    <div className='form-container'>
+       
+            //        <div className='message-container'>
+            //             <div className='message'>
+            //                 <p>
+            //                     <b>Fit Cooking Back Into Your Schedule with </b>
+            //                     <img src='./logo-blk.png' alt="bug" height={40} />
+            //                 </p>
+            //             </div>
+            //         </div>
+            //         <div className="form-card">
+            //             <BasicExample />
+            //             </div>
+            //     </div>
+
+            <Container fluid>
+                <Row>
+                    <Col>
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+                );
+
+}
+export default Login;
