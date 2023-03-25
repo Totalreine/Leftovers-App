7984ef984d38f6b8ceb87d11797e9f6d1706c6ec
Hibaq Warsame <hibaq@ualberta.ca>
2023-03-24 22:31:16 -0600

    made login page responsive

--- Frontend/src/components/FormComp.js
+++ Frontend/src/components/FormComp.js
@@ -1,33 +1,42 @@
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function BasicExample() {
 
   return (
-    <Form className = "form">
-      <div className='lglabel'><h5>Login</h5></div>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container>
+      <Row>
+        <Col>
+          <Form className="form">
+            <div className='lglabel'><h5>Login</h5></div>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password"/>
-      </Form.Group>
-      <div className= "btn-div">
-      <Button variant="primary" style={{backgroundColor: '#F29544', borderColor:'#F29544', padding: '10px 166px' }} type="submit">
-        Login
-      </Button>
-      </div>
-      <div className="signup">
-      <Link to="/signup" className="signup-link">Don’t have an account yet? Sign Up</Link>
-      </div>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Row>
+             <Col>
+              <Button variant="primary" style={{ backgroundColor: '#F29544', borderColor: '#F29544'}} type="submit">
+                Login
+              </Button>
+            <div className="signup">
+              <Link to="/signup" className="signup-link">Don’t have an account yet? Sign Up</Link>
+            </div>
+            </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- Frontend/src/routes/Login.js
+++ Frontend/src/routes/Login.js
@@ -1,29 +1,30 @@
-import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Login.css"
 import BasicExample from '../components/FormComp';
 function Login() {
     return (
-            <Container fluid>
-            <div className='row g-0'>
+        <div className='login-page'>
+                <div className='row g-0'>
                     <Col id='message-container'>
-                    <div className='message'>
-                             <p>
-                                <b>Fit Cooking Back Into Your Schedule with </b>
-                               <img src='./logo-blk.png' alt="bug" height={40} />
-                            </p>
-                         </div>
-                
+                            < div className='align-self-center' id='message'>
+                                <p>
+                                    <b>Fit Cooking Back Into Your Schedule with </b>
+                                    </p>
+                                    <img src='./logo-blk.png' alt="bug" height={40} />
+                               
+                            </div>
+
                     </Col>
                     <Col id='login-form'>
-                        <BasicExample/>
+                         <BasicExample/>
+                        
                     </Col>
                 </div>
-                </Container>
-       
-                );
-               
+        </div>
+
+    );
+
 
 }
 export default Login;
