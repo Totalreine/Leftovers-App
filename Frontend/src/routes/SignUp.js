71ec06b27edb1ba1971a04cebabfb3a77703167f
woodenonesie <olga.pi4ugina@yandex.ru>
2023-03-28 00:54:54 -0700

    alter welcome page

--- Frontend/src/routes/SignUp.js
+++ Frontend/src/routes/SignUp.js
@@ -2,26 +2,26 @@ import SignUpForm from "../components/SignUpForm";
 import Col from 'react-bootstrap/Col';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./signup-login.css"
-import NavBar from '../components/NavBar';
+import RootNavBar from '../components/RootNavBar';
 
 function SignUp() {
   return (
     <div className='signup-page'>
-      <NavBar />
+      <RootNavBar />
       <div className='row g-0'>
         <Col id='message-container'>
           < div id='message'>
             <p>
-              <b>Fit Cooking Back Into Your Schedule with </b>
+              <b>Fit Cooking Back</b>
+            </p>
+            <p>
+              <b>Into Your Schedule with</b>
             </p>
             <img src='./logo-blk.png' alt="leftovers-logo" height={40} />
-
           </div>
-
         </Col>
         <Col id='signup-form'>
           <SignUpForm />
-
         </Col>
       </div>
     </div>
